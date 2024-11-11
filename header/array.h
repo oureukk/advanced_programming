943e2bef44f39a796a37c193a3b6274f6e352016
oureukk <whalsrb1203@kookmin.ac.kr>
2024-11-12 01:50:54 +0900

    Add files via upload

---
+++ header/array.h
@@ -0,0 +1,13 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <stdint.h>  
+#include "bi_struct.h"
+
+typedef uint32_t word;
+
+msg array_init(word* a, int word_len);
+msg array_copy(word** dst, const word* src, int word_len);
+msg array_rand(word* dst, int word_len);
+
+#endif 

e07aaa6bca4f41cfea4970c3e61ee6b49faf7e83
oureukk <whalsrb1203@kookmin.ac.kr>
2024-11-12 01:52:22 +0900

    Update bi_arith.h

--- header/bi_arith.h
+++ header/bi_arith.h
@@ -1,11 +1,11 @@
-#ifndef bi_function_H
-#define bi_function_H
+#ifndef bi_arith_H
+#define bi_arith_H
 
 #include "bi_struct.h"
 #include "bi_function.h"
 
-msg bi_add(pbigint* result, const pbigint a, const pbigint b);
+void bi_add(pbigint* result, const pbigint a, const pbigint b);
 int bi_compare_abs(const pbigint a, const pbigint b);
-msg bi_subtract(pbigint* result, const pbigint a, const pbigint b);
+void bi_subtract(pbigint* result, const pbigint a, const pbigint b);
 
 #endif

868c51b8fc03fa5b6d0fb5ed4f03213ccbf4d405
oureukk <whalsrb1203@kookmin.ac.kr>
2024-11-11 18:54:34 +0900

    Update bi_arith.h
    add 헤더 추가

--- bi_arith.h
+++ bi_arith.h
@@ -4,6 +4,7 @@
 #include "bi_struct.h"
 #include "bi_function.h"
 
+msg bi_add(pbigint* result, const pbigint a, const pbigint b);
 int bi_compare_abs(const pbigint a, const pbigint b);
 msg bi_subtract(pbigint* result, const pbigint a, const pbigint b);
 

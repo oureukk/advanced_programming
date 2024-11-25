273f33f0cb319d10c5d9cd7e61af28dd4fff3bb9
oureukk <whalsrb1203@kookmin.ac.kr>
2024-11-26 00:34:36 +0900

    Update bi_function.h

--- header/bi_function.h
+++ header/bi_function.h
@@ -8,10 +8,12 @@ msg bi_new(pbigint* dst, int word_len);
 msg bi_set_from_array(pbigint* dst, int sign, int word_len, word* a);
 msg bi_set_from_string(pbigint* dst, const char* str, int base);
 msg bi_get_random(pbigint* dst, int word_len);
-msg bi_delete(pbigint* dst);
+void bi_delete(pbigint* dst);
 msg bi_refine(pbigint* dst);
 msg bi_assign(pbigint* dst, const pbigint* src);
 msg bi_print(pbigint* dst, int base);
+void bi_shift_left(pbigint* result, const pbigint A, int shift);
+void bi_shift_right(pbigint* result, const pbigint src, int shift);
 
 
 

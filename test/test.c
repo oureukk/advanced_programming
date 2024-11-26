451bc962c1094f5edc9b965273cf0e9d0ec55fb8
yoseob-shim <yosuob030502@kookmin.ac.kr>
2024-11-26 19:43:15 +0900

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "bi_function.c"
-#include "bi_arith.c"
-#include "array.c"
+#include "bi_function.h"
+#include "bi_arith.h"
+#include "array.h"
 
 void test_write(pbigint* pnum, FILE* fp) {
     if ((*pnum)->sign == -1) {
@@ -16,7 +16,7 @@ void test_write(pbigint* pnum, FILE* fp) {
             continue; // 처음 유효한 값 전까지 0 생략
         }
         first = 0;
-        fprintf(fp, "%x", (*pnum)->a[i]);
+        fprintf(fp, "%08x", (*pnum)->a[i]);
     }
 
     if (first) { // 모든 값이 0인 경우
@@ -27,18 +27,18 @@ void test_write(pbigint* pnum, FILE* fp) {
 }
 
 void test_check(pbigint* pnum1, pbigint* pnum2, pbigint* result, int oper) {
-    const char* fname = "test.txt"; 
+    const char* fname = "test.txt";
     static int first_call = 1;
     FILE* fp;
-    
-    if (first_call) {
-        fopen_s(&fp, fname, "w");  
-        first_call = 0; 
-    }
-    else 
-        fopen_s(&fp, fname, "a");  
 
-    
+    if (first_call) {
+        fopen_s(&fp, fname, "w");
+        first_call = 0;
+    }
+    else
+        fopen_s(&fp, fname, "a");
+
+
     if (fp == NULL) {
         perror("파일 열기 실패");
         return;
@@ -47,7 +47,7 @@ void test_check(pbigint* pnum1, pbigint* pnum2, pbigint* result, int oper) {
     test_write(pnum1, fp);
 
     test_write(pnum2, fp);
-    
+
     test_write(result, fp);
 
     fprintf(fp, "%d\n", oper);
@@ -56,31 +56,42 @@ void test_check(pbigint* pnum1, pbigint* pnum2, pbigint* result, int oper) {
     fclose(fp);
 }
 
-int main() {
 
+void test_ran()
+{
     pbigint a = NULL;
     pbigint b = NULL;
     pbigint result1 = NULL;
+    pbigint result2 = NULL;
 
-    int large_word_len = 10;
+    int large_word_len = 1920/sizeof(word);
 
     // 큰 정수 초기화
     bi_get_random(&a, large_word_len);
     bi_get_random(&b, large_word_len);
 
-    // 덧셈과 뺄셈 수행
-    bi_add(a,b,&result1);
-
-
-    // 결과를 파일에 기록
+     //덧셈과 뺄셈 수행
+    bi_add(a, b, &result1);
     test_check(&a, &b, &result1, 1); // 덧셈 결과 기록
 
+    SUB(a, b, &result1);
+    test_check(&a, &b, &result1, 2); 
 
+    //MUL(a, b, &result1);
+    //test_check(&a, &b, &result1, 3);
     // 메모리 해제
     bi_delete(&a);
     bi_delete(&b);
     bi_delete(&result1);
 
+}
+
+int main() {
+
+    for (int i = 0; i < 10; i++)
+    {
+        test_ran();
+    }
 
     return 0;
 }

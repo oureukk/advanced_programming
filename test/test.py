fbdc6eacf4e7a6802dc1d7ef65ab7dd6dcd7cb1d
yoseob-shim <yosuob030502@kookmin.ac.kr>
2024-12-05 05:18:16 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,3 @@
-
 try:
     f = open("test.txt", 'r')
     lines = f.readlines()
@@ -13,89 +12,73 @@ try:
 
     def remove_leading_zeros(num_str):
         if num_str.startswith('-'):
-            # 음수인 경우, '-'를 제외한 나머지 부분의 '0'을 제거한 후 '-'와 합칩니다.
-            return '-' + num_str[1:].lstrip('0') or '0'
+            stripped = num_str[1:].lstrip('0')
+            return '-0' if stripped == '' else '-' + stripped
         else:
-            # 양수인 경우, 그냥 '0'을 제거합니다.
-            return num_str.lstrip('0') or '0'
+            stripped = num_str.lstrip('0')
+            return '0' if stripped == '' else stripped
+
     i = 0
     j = 0
     count = 0
     for line in lines:
 
         num1,num2,result,oper=line.split()
-        num1=remove_leading_zeros(num1)
-        num2=remove_leading_zeros(num2)
-        result=remove_leading_zeros(result)
-
+        num1= remove_leading_zeros(num1)
+        num2= remove_leading_zeros(num2)
+        result=remove_leading_zeros(result) 
         num1 = int(num1,16)
         num2 = int(num2,16)
         count = count +1
-        
-        
 
         if oper == "1":
             compute=remove_hex_prefix(hex(num1 + num2))
-
             if(result == compute):
                 i=i+1
-                print("check1",count)
+                print("check plus",count)
                 continue
             else:
                 j=j+1
-                print("wrong compute1",count)
+                print("wrong compute plus",count)
                 print("파이썬 계산\t", compute)
                 print("C 계산\t\t", result)
-
-                
         elif oper == "2":
             compute=remove_hex_prefix(hex(num1- num2))
-
-
             if(result == compute):
                 i=i+1
-                print("check2",count)
                 continue
-
             else:
                 j=j+1
-                print("wrong compute2",count)
+                print("wrong compute sub",count)
                 print("파이썬 계산\t",compute)
                 print("C 계산\t\t", result)
-                
-
-                
         elif oper == "3":
             compute=remove_hex_prefix(hex(num1* num2))
-
             if(result == compute):
                 i=i+1
                 print("check3",count)
                 continue
             else:
                 j=j+1
-                print("wrong compute3",count)
-                print(f'line : {f.tell()}')
+                print("wrong compute MUL",count)
                 print("파이썬 계산\t",compute)
-                print("C 계산\t\t", result)
-                
-                
-                
+                print("C 계산\t\t", result)  
         elif oper == "4":
-            compute=remove_hex_prefix(hex(int(num1/ num2)))
-
-            
+            if num1==0 or num2 ==0:
+                compute = "0"
+            else:
+                compute=remove_hex_prefix(hex(int(num1/ num2)))
             if(result == compute):   
                 i=i+1
                 print("check4",count)
                 continue
             else:
                 j=j+1
-                print("wrong compute4",count)
+                print("wrong compute DIV",count)
                 print("파이썬 계산\t", compute)
                 print("C 계산\t\t", (result))
                 
-
+ 
     print("맞은 숫자",i)
     print("틀린 숫자",j)
     print("fin")
@@ -106,3 +89,5 @@ except FileNotFoundError:
     print("파일을 찾을 수 없습니다!")
 except IOError:
     print("파일을 읽는 동안 오류가 발생했습니다!")
+
+

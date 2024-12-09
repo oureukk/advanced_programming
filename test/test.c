d685bd025e2af973d5921db7b2aa626af2bf0994
sukim22 <kiffe02@kookmin.ac.kr>
2024-12-09 13:43:11 +0900

    Add files via upload

--- test/test.c
+++ test/test.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "bi_function.h"
@@ -56,21 +56,55 @@ void test_check(pbigint* pnum1, pbigint* pnum2, pbigint* result, int oper) {
     fclose(fp);
 }
 
+void test_check_exp(pbigint* pnum1, pbigint* pnum2, pbigint* pnum3, pbigint* result, int oper) {
+    const char* fname = "test2.txt";
+    static int first_call = 1;
+    FILE* fp;
 
+    if (first_call) {
+        fopen_s(&fp, fname, "w");
+        first_call = 0;
+    }
+    else
+        fopen_s(&fp, fname, "a");
+
+
+    if (fp == NULL) {
+        perror("파일 열기 실패");
+        return;
+    }
+
+    test_write(pnum1, fp);
+
+    test_write(pnum2, fp);
+
+    test_write(pnum3, fp);
+
+    test_write(result, fp);
+
+    fprintf(fp, "%d\n", oper);
+
+    printf("write OK\n");
+    fclose(fp);
+}
 
 void test_ran()
 {
     pbigint a = NULL;
     pbigint b = NULL;
+    pbigint c = NULL;
     pbigint result1 = NULL;
     pbigint result2 = NULL;
+    pbigint result3 = NULL;
+    pbigint result4 = NULL;
 
-    int large_word_len = 1920 / sizeof(word);
+    int large_word_len = 256 / sizeof(word);
     //1920/sizeof(word)
 
     // 큰 정수 초기화
     bi_get_random(&a, large_word_len);
     bi_get_random(&b, large_word_len);
+    bi_get_random(&c, large_word_len);
     
     clock_t start,end;
     double cpu_time_used;
@@ -98,15 +132,31 @@ void test_ran()
     DIVC(a, b, &result1, &result2);
     test_check(&a, &b, &result1, 4);
 
+    start = clock();
+    ltr(a, b, c, &result3);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("LTR Time: %f seconds\n", cpu_time_used);
+    test_check_exp(&a, &b, &c, &result1, 5);
+
+    start = clock();
+    rtl(a, b, c, &result4);
+    end = clock();
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("RTL Time: %f seconds\n", cpu_time_used);
+    test_check_exp(&a, &b, &c, &result1, 6);
+
     // 메모리 해제
     bi_delete(&a);
     bi_delete(&b);
+    bi_delete(&c);
     bi_delete(&result1);
     bi_delete(&result2);
-
+    bi_delete(&result3);
+    bi_delete(&result4);
 }
 
-/*int main() {
+int main() {
     srand((unsigned int)time(NULL));
     for (int i = 0; i < 10; i++)
     {
@@ -114,11 +164,10 @@ void test_ran()
 
     }
     return 0;
-}*/
+}
 
-// rtl, ltr, bi_mod 테스트 함수 메모리 문제 발생
-// bi_function.c 내 bi_new, bi_delete, bi_assign, bi_print 부분
-int main() {
+// rtl, ltr, bi_mod 테스트 함수
+/*int main() {
     pbigint base = NULL;
     pbigint exponent = NULL;
     pbigint modulus = NULL;
@@ -127,25 +176,6 @@ int main() {
 
     // 큰 정수 초기화
     int large_word_len = 1920 / sizeof(word);
-
-    // 랜덤 값으로 base, exponent, modulus 초기화
-    /*if (bi_get_random(&base, large_word_len) != 0) {
-        fprintf(stderr, "Failed to initialize base\n");
-        return 1;
-    }
-
-    if (bi_get_random(&exponent, large_word_len) != 0) {
-        fprintf(stderr, "Failed to initialize exponent\n");
-        bi_delete(&base);  // base 메모리 해제
-        return 1;
-    }
-
-    if (bi_get_random(&modulus, large_word_len) != 0) {
-        fprintf(stderr, "Failed to initialize modulus\n");
-        bi_delete(&base);  // base 메모리 해제
-        bi_delete(&exponent);  // exponent 메모리 해제
-        return 1;
-    }*/
 
     bi_get_random(&base, large_word_len);
     bi_get_random(&exponent, large_word_len);
@@ -156,11 +186,9 @@ int main() {
 
     // LTR 모듈러 거듭제곱 계산
     ltr(base, exponent, modulus, &result);
-    
     clock_t end_time = clock();
     double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
     printf("ltr function took %.4f seconds to execute\n", time_taken);
-    
     // 결과가 있다면 결과 출력
     if (result != NULL) {
         printf("Result: ");
@@ -171,15 +199,19 @@ int main() {
     // RTL 모듈러 거듭제곱 계산
     rtl(base, exponent, modulus, &result2);
 
+    clock_t end_time = clock();
+    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("rtl function took %.4f seconds to execute\n", time_taken);
+
     // 결과가 있다면 결과 출력
-    if (result != NULL) {
+    if (result2 != NULL) {
         printf("Result: ");
         bi_print(&result2, 16); // 결과 출력 (16진수)
     }
     else printf("no result error\n");
 
-    // bi_mod 메모리 문제 발생
-    //bi_mod(base, exponent, modulus);
+    // 문제 없음
+    //bi_mod(base, exponent, &modulus);
 
     // 메모리 해제
     bi_delete(&base);
@@ -189,4 +221,4 @@ int main() {
     bi_delete(&result2);
 
     return 0;
-}
+}*/
